640462378e0f40a8ffac5a9c2b43eb40700728d2
Daniel <daniel.dungngo@gmail.com>
2017-12-05 16:06:20 +0700

    update mlabconfig

--- src/controllers/MgConfig.ts
+++ src/controllers/MgConfig.ts
@@ -2,7 +2,7 @@ import * as mongoose from "mongoose";
 
 export default class MgConfig {
     constructor(
-        private mongodbURI = "mongodb://localhost/form_database",
+        private mongodbURI = "mongodb://surveytest:123123@ds129776.mlab.com:29776/form_database",
         private options = {
             keepAlive: 300000,
             connectTimeoutMS: 30000,
