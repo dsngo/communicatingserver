7767be7cd564f5afbc33861d5e596b3bb3ea5222
Daniel <daniel.dungngo@gmail.com>
2018-05-03 16:57:28 +0700

    heroku tes 2

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,12 @@ import { customCORS } from "./controllers/headerController";
 import routeSurvey from "./routes/routeSurvey";
 import routeClientSurvey from "./routes/routeClientSurvey";
 import routeTest from "./routes/routeTest";
-import routePeakvn from "./routes/routePeakvn"
+import routePeakvn from "./routes/routePeakvn";
 
 const PORT: any = process.env.PORT || 3000;
-const IP: any = process.env.IP;
-const LOG = () => console.log(`Server is listening... ${IP || "localhost"}:${PORT}`); // tslint:disable-line
+const IP: any = process.env.IP || "0.0.0.0";
+const LOG = () =>
+  console.log(`Server is listening... ${IP || "localhost"}:${PORT}`); // tslint:disable-line
 
 const app: express.Application = express();
 // Connect to Database
@@ -21,10 +22,12 @@ app.use(customCORS);
 // app.use("/survey", routeSurvey);
 // app.use("/client-survey", routeClientSurvey);
 // app.use("/test", routeTest);
-app.use("/peak-vn/ecsite", routePeakvn)
+app.use("/peak-vn/ecsite", routePeakvn);
 
 app.get("/", (rq, rs) =>
-  rs.send("This is a node JS server for api fetching. Nothing too interesting here. If you are interest go to fb.com/DanielDNgo. PS: Im coool!"),
+  rs.send(
+    "This is a node JS server for api fetching. Nothing too interesting here. If you are interest go to fb.com/DanielDNgo. PS: Im coool!",
+  ),
 );
 console.log(process.env.PORT);
 
