bfe91eb08b44d532d3382c42da53547aea966aa3
Daniel <daniel.dungngo@gmail.com>
2018-06-18 13:55:25 +0700

    heroku deploy

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import app from "./server";
 declare const module: any;
 
 const PORT: any = process.env.PORT || 3000;
-const IP: any = process.env.IP || "0.0.0.0";
+const IP: any = process.env.IP || "0.0.0.0"; // use '0.0.0.0' for heroku because f0ck heroku
 const LOG = () =>
   console.log(`Server is listening... ${IP || "0.0.0.0"}:${PORT}`); // tslint:disable-line
 const server = http.createServer(app as any);
